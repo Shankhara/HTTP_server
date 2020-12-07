5544f86be6fc7bc62dfa797c281bf761a6565de0
cbouleng <boulenge.charles28@gmail.com>
2020-12-07 15:45:38 +0100

    adjusting test

--- server/unittests/unittests.hpp
+++ server/unittests/unittests.hpp
@@ -8,11 +8,14 @@ void testRequest()
 {
 	Client c(0);
 
-	std::string get = "GET /http://localhost:8080 HTTP/1.1\r\n";
+	std::string get = "GET /http://localhost:8080 HTTP/1.1\r\nUser-Agent: 1\r\nReferer: 2\r\nContent-Type: 3\r\n\r\n";
+//FAIL	std::string get = "GET /http://localhost:8080 HTTP/1.1\r\nUser-Agent: 1\r\nReferer: 2\r\nContent-Type: 3\r\nBonjour\r\n";
+//FAIL	std::string get = "GETGET /http://localhost:8080 HTTP/1.1\r\n";
+//FAIL	std::string get = "GET GET /http://localhost:8080 HTTP/1.1\r\n";
 	c.constructRequest(const_cast<char *>(get.c_str()), get.size());
 
 	Request r(c);
-	assertEqual(r.parse(), 1, "parseRequestLine");
+	assertEqual(r.parse(), 0, "parseRequestLine");
 }
 
 void testClient()
