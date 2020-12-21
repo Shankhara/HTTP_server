22affcd9797b3462d784d1ccb1ff48af47e4b5d2
kh42z <tterrail@student.42.fr>
2020-12-21 19:16:46 +0100

    google coding main.cpp

--- server/main.cpp
+++ server/main.cpp
@@ -11,21 +11,22 @@ void signalHandler(int) {
 	exit(0);
 }
 
-void addListener(const Parsing::server &server, std::vector<Listener*> &listeners)
+void addListener(const Parsing::server &server, std::vector<Listener*> *listeners)
 {
-	for (unsigned long i = 0; i < listeners.size(); i++)
+	for (uint64_t i = 0; i < listeners->size(); i++)
 	{
-		if (listeners[i]->addServer(server) == 0)
-			return ;
+		if (listeners->at(i)->addServer(server) == 0) {
+			return;
+		}
 	}
 	Listener *l = new Listener();
 	l->addServer(server);
-	listeners.push_back(l);
+	listeners->push_back(l);
 }
 
 void startListeners(const std::vector<Listener*> &listeners)
 {
-	for (unsigned long i = 0; i < listeners.size(); i++)
+	for (uint64_t i = 0; i < listeners.size(); i++)
 	{
 		listeners[i]->ListenAndServe();
 		Server::getInstance()->addFileDescriptor(listeners[i]);
@@ -38,10 +39,12 @@ int main(int argc, char *argv[]) {
 	signal(SIGCHLD,SIG_IGN);
 	signal(SIGINT, signalHandler);
 	std::string conf("./parsing/test/wordpress.conf");
-	if (argc > 1 && std::string(argv[1]).compare("-v") == 0)
+	if (argc > 1 && std::string(argv[1]).compare("-v") == 0){
 		Log::setLevel(logDEBUG);
-	else if (argc > 1)
+	}
+	else if (argc > 1) {
 		conf = std::string(argv[1]);
+	}
 	Parsing p = Parsing(conf);
 	try {
 		p.parseConfig();
@@ -50,8 +53,9 @@ int main(int argc, char *argv[]) {
 		return (EXIT_FAILURE);
 	}
 	Server *webserv = Server::getInstance();
-	for (size_t i = 0; i < p.getServers().size(); i++)
-		addListener(p.getServers()[i], listeners);
+	for (size_t i = 0; i < p.getServers().size(); i++) {
+		addListener(p.getServers()[i], &listeners);
+	}
 	startListeners(listeners);
 	webserv->start();
 }
