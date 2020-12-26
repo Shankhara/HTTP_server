1cab0a36a61bee558b43b0be438da833f6322151
kh42z <tterrail@student.42.fr>
2020-12-26 21:03:01 +0100

    rollback main default logging

--- server/main.cpp
+++ server/main.cpp
@@ -39,8 +39,9 @@ int main(int argc, char *argv[]) {
 	signal(SIGCHLD,SIG_IGN);
 	signal(SIGINT, signalHandler);
 	std::string conf("./parsing/test/wordpress.conf");
-	Log::setLevel(logDEBUG);
-	if (argc > 1) {
+	if (argc > 1 && std::string(argv[1]).compare("-v") == 0) {
+		Log::setLevel(logDEBUG);
+	} else if (argc > 1) {
 		conf = std::string(argv[1]);
 	}
 	Parsing p = Parsing(conf);
