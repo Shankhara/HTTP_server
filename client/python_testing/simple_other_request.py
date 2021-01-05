0985b7f1614ee5f5c5e089768b271cd0245ce5a4
nerap <raph.en223@gmail.com>
2021-01-05 14:23:17 +0100

    return application/octet-stream when get a file

--- client/python_testing/simple_other_request.py
+++ client/python_testing/simple_other_request.py
@@ -24,28 +24,67 @@ class Test_other_request():
 		self.tests = pt().test(webserv.text, "", "Checking for empty content", self.tests)
 
 	def test02_put(self):
-		self.print_test_value("/post/test", "PUT", "02")
-		webserv = req.put(self.url_webserv + "/post/test", data="put content")	
-		self.tests = pt().test(str(webserv.status_code), "200", "Checking status code", self.tests)
-		webserv = req.get(self.url_webserv + "/post/test")
-		self.tests = pt().test(str(webserv.status_code), "200", "Checking status code", self.tests)
-		self.tests = pt().test(webserv.text, "put content", "Checking content of get after put request", self.tests)
+		self.print_test_value("/{number}", "PUT (creating file)", "02")
+		for i in range (50):
+			webserv = req.put(self.url_webserv + "/" + str(i), data="put content unchanged")	
+			self.tests = pt().test(str(webserv.status_code), "201", "Checking status code", self.tests)
+			webserv = req.get(self.url_webserv + "/" + str(i))
+			self.tests = pt().test(str(webserv.status_code), "200", "Checking status code", self.tests)
+			self.tests = pt().test(webserv.text, "put content unchanged", "Checking content of get after put request", self.tests)
+
+	def test03_put(self):
+		self.print_test_value("/{number}", "PUT (modifying file)", "03")
+		for i in range (50):
+			webserv = req.put(self.url_webserv + "/" + str(i), data="put content")	
+			self.tests = pt().test(str(webserv.status_code), "200", "Checking status code", self.tests)
+			webserv = req.get(self.url_webserv + "/" + str(i))
+			self.tests = pt().test(str(webserv.status_code), "200", "Checking status code", self.tests)
+			self.tests = pt().test(webserv.text, "put content", "Checking content of get after put request", self.tests)
+
+
+	def test04_put(self):
+		self.print_test_value("/post/{number}", "PUT (modifying file)", "04")
+		for i in range (50):
+			webserv = req.put(self.url_webserv + "/post/" + str(i), data="put content")	
+			self.tests = pt().test(str(webserv.status_code), "201", "Checking status code", self.tests)
+			webserv = req.get(self.url_webserv + "/post/" + str(i))
+			self.tests = pt().test(str(webserv.status_code), "200", "Checking status code", self.tests)
+			self.tests = pt().test(webserv.text, "put content", "Checking content of get after put request", self.tests)
+
+	def test05_delete(self):
+		self.print_test_value("/{number}", "DELETE", "05")
 	
-	def test03_delete(self):
-		self.print_test_value("/post/test", "DELETE", "03")
-		test_url_webserv = self.url_webserv + "/post/test";	
-		
-		res = req.get(test_url_webserv)
-		self.tests = pt().test(str(res.status_code), "200", "Checking status code of get /post/test", self.tests)
-		self.tests = pt().test(res.text, "delete and put content", "Checking content of get /post/test", self.tests)
-		webserv = req.delete(test_url_webserv)	
-		self.tests = pt().test(str(webserv.status_code), "200", "Checking status code of get /post/test", self.tests)	
-		res = req.get(test_url_webserv)
-		self.tests = pt().test(str(res.status_code), "404", "Checking status code of get after deleting file", self.tests)
+		for i in range (50):	
+			test_url_webserv = self.url_webserv + "/" + str(i);	
+			res = req.get(test_url_webserv)
+			self.tests = pt().test(str(res.status_code), "200", "Checking status code of get /" + str(i), self.tests)
+			self.tests = pt().test(res.text, "put content", "Checking content of get /" + str(i), self.tests)
+			webserv = req.delete(test_url_webserv)	
+			self.tests = pt().test(str(webserv.status_code), "204", "Checking status code of get /" + str(i), self.tests)	
+			res = req.get(test_url_webserv)
+			self.tests = pt().test(str(res.status_code), "404", "Checking status code of get after deleting file", self.tests)
+
+	def test06_delete(self):
+		self.print_test_value("/post/{number}", "DELETE", "06")
+	
+		for i in range (50):	
+			test_url_webserv = self.url_webserv + "/post/" + str(i);	
+			res = req.get(test_url_webserv)
+			self.tests = pt().test(str(res.status_code), "200", "Checking status code of get /" + str(i), self.tests)
+			self.tests = pt().test(res.text, "put content", "Checking content of get /" + str(i), self.tests)
+			webserv = req.delete(test_url_webserv)	
+			self.tests = pt().test(str(webserv.status_code), "204", "Checking status code of get /" + str(i), self.tests)	
+			res = req.get(test_url_webserv)
+			self.tests = pt().test(str(res.status_code), "404", "Checking status code of get after deleting file", self.tests)
+
+
 
 	def test_simple_other_request(self):
 	#	self.test00_trace()
 		self.test01_head()
 		self.test02_put()
-		self.test03_delete()
+		self.test03_put()
+		self.test04_put()
+		self.test05_delete()
+		self.test06_delete()
 		return self.tests	
