c8b8ae92639da4a61bed79ccb4389a47c5bbd302
nerap <raph.en223@gmail.com>
2021-01-08 12:27:46 +0100

    removing unsued line in parsing .conf

--- server/unittests/testParsing.cpp
+++ server/unittests/testParsing.cpp
@@ -39,7 +39,6 @@ void testParsing()
 	assertThrow("./parsing/duplicateLocation.conf", "adding the same location twice");
 	assertThrow("./parsing/duplicateRoot.conf", "adding the same root twice");
 	assertThrow("./parsing/invalidPort.conf", "[emerg] invalid port");
-	assertThrow("./parsing/duplicateServerName.conf", " duplicate server name");
 	assertThrow("./parsing/doubleSemicolon.conf", " duplicate semicolon");
 	assertThrow("./parsing/noServer.conf", " no server provide");
 	assertThrow("./parsing/noArguments.conf", " no arguments provide");
